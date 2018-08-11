7779a2334f89fc88bf272ae357867dccf60401f9
daf <daf@madalien.com>
2018-08-11 15:22:57 +0100

    Updated instructions.txt and version

--- bnetlauncher/Properties/AssemblyInfo.cs
+++ bnetlauncher/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.22.*")]
+[assembly: AssemblyVersion("1.99.*")]
 //[assembly: AssemblyFileVersion("1.22.*")]
