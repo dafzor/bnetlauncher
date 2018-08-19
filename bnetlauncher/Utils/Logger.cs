511e747e41ebd4f9c9db3ae4691e6ffc83a6a690
daf <daf@madalien.com>
2018-08-19 01:43:58 +0100

    Added color to console output * added color to console output * added todo comment

--- bnetlauncher/Utils/Logger.cs
+++ bnetlauncher/Utils/Logger.cs
@@ -1,11 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+
+/// <summary>
+/// TODO:
+/// - support multiple programs writing to the file at the same time
+/// - make it so that blocked text file doesn't halt execution
+/// - add old log cleanup/trimming code
+/// - make code more robust?
+/// - something i'm probably forgeting
+/// </summary>
 
 namespace bnetlauncher.Utils
 {
@@ -58,13 +63,8 @@ namespace bnetlauncher.Utils
             }
             if (OutPutToConsole)
             {
-                Console.WriteLine(line);
-                if (ex != null)
-                {
-                    Console.Write(ex.ToString());
-                }
+                ConsoleWrite(message, ex, src_path, src_member, src_line, type);
             }
-
         }
 
         private static void WriteLog(string line)
@@ -72,6 +72,47 @@ namespace bnetlauncher.Utils
             StreamWriter file = new StreamWriter(log_file, true);
             file.WriteLine(line);
             file.Close();
+        }
+
+        private static void ConsoleWrite(string message, Exception ex, string src_path, string src_member, int src_line, string type)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(DateTime.Now.ToString("HH:mm:ss.ffff"));
+
+            Console.ResetColor(); Console.Write("|");
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write(Process.GetCurrentProcess().Id);
+
+            Console.ResetColor(); Console.Write("|");
+
+            switch (type)
+            {
+                case "error":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "warn":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    break;
+            }
+
+            Console.Write($"{GetSrcFile(src_path)}.{src_member}:{src_line}|{type}");
+
+            Console.ResetColor(); Console.Write("|");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(message);
+
+            if (ex != null)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.ResetColor();
         }
 
         private static string GetSrcFile(string path)
