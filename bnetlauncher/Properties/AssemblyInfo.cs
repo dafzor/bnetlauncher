22ce77dbcb9a40bcecd0c4b7861d2126d76627a3
Amelia Magee <33827951+Sectimus@users.noreply.github.com>
2023-03-17 12:10:26 +0000

    Added support for Diablo IV Beta (#86)
    * Added support for Diablo IV Beta
    * Updated version information

--- bnetlauncher/Properties/AssemblyInfo.cs
+++ bnetlauncher/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.18.*")]
-//[assembly: AssemblyFileVersion("2.18.*")]
+[assembly: AssemblyVersion("2.19.*")]
+//[assembly: AssemblyFileVersion("2.19.*")]
 [assembly: NeutralResourcesLanguage("en")]
